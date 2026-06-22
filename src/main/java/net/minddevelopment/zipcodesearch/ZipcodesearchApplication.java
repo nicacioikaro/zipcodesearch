acee2524025b9c005e57bc496ad6ffb0766f83d2
Ikaro Nicacio <ikaro.dev@gmail.com>
2026-06-22 03:00:25 -0300

    chore: initial commit

---
+++ src/main/java/net/minddevelopment/zipcodesearch/ZipcodesearchApplication.java
@@ -0,0 +1,14 @@
+package net.minddevelopment.zipcodesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+public class ZipcodesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipcodesearchApplication.class, args);
+	}
+
+}
